c030998717e61f4a6dc92238a3d105483e5374a2
Harri <Harri.g.owen@gmail.com>
2018-06-10 16:22:30 +0100

    Update shipping-payment-mixin.js

--- view/frontend/web/js/view/shipping-payment-mixin.js
+++ view/frontend/web/js/view/shipping-payment-mixin.js
@@ -1,16 +1,19 @@
 define(
     [
-        'ko'
-    ], function (ko) {
+        'ko',
+		'Magento_Customer/js/model/customer'
+    ], function (ko, customer) {
         'use strict';
 
         var mixin = {
 
             initialize: function () {
-				this.isVisible = ko.observable(false);
-                this.visible = ko.observable(false); // set visible to be initially false to have your step show first
-                this._super();
-
+				if(!customer.isLoggedIn()) {
+					this.isVisible = ko.observable(false);
+					this.visible = ko.observable(false); // set visible to be initially false to have your step show first
+				}
+					this._super();
+				
                 return this;
             }
         };
